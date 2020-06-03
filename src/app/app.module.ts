380acdfca87ff8d67e85bbeacff1e6a2a8311421
Allan Torres <allan.torres@arce.ce.gov.br>
2020-06-03 20:59:26 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuprimeiroComponente } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuprimeiroComponente
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'primeiro-componente',
+    template: `<h1> hello world</h1>`
+})
+export class MeuprimeiroComponente implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
